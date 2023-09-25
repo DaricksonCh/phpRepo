991e7b49c6cf094a4e8d2752398bb77e8b2d90f6
DaricksonCh <darickson.stiven96@gmail.com>
2023-09-24 22:03:50 -0500

    laravel

---
+++ mas-control/resources/views/publicacion/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.auth-master')
+
+@section('content')
+  <form action="" method="POST">
+    @csrf
+    <h1>Crear publicacion</h1>
+    @include('layouts.partials.messages')
+    <div class="mb-3 form-floating">
+      <input type="email" name="email" placeholder="Correo Electronico" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
+    </div>
+    <div class="mb-3 form-floating">
+      <input type="password" placeholder="Conmtraseña" name="password" class="form-control" id="exampleInputPassword1">
+      <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+    </div>
+    <div class="mb-3 form-floating">
+      <a href="/register">Crea un usuario</a>
+    </div>
+    <button type="submit" value="Login" class="btn btn-primary">Enviar</button>
+  </form>
+@endsection
+
